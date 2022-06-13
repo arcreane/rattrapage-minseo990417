37926f6461703b9739b2e890f804735c3f84890c
minseochoi <kelly00417@naver.com>
2022-06-14 01:19:13 +0900

    Initial Commit

---
+++ 2.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2.1
+//
+//  Created by 최민서 on 2022/06/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
